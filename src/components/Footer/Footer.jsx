8c9aecd6e50a1389c0029bc24d9255c04df902e8
Tram Nguyen <tramngoc1402@gmail.com>
2022-08-25 20:14:49 +0300

    Finished Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Container, Row, Col, ListGroup, ListGroupItem } from 'reactstrap';
 import logo from '../../assets/images/res-logo.png';
 import '../../styles/footer.css';
@@ -32,8 +33,60 @@ const Footer = () => {
               </ListGroupItem>
             </ListGroup>
           </Col>
-          <Col lg='3' md='4' sm='6'></Col>
-          <Col lg='3' md='4' sm='6'></Col>
+          <Col lg='3' md='4' sm='6'>
+            <h5 className='footer__title'>Contact</h5>
+            <ListGroup className='delivery__time-list'>
+              <ListGroupItem className='delivery__time-item border-0 ps-0'>
+                <span>Location: 123 Olympiakatu, Espoo, FI</span>
+              </ListGroupItem>
+
+              <ListGroupItem className='delivery__time-item border-0 ps-0'>
+                <span>Hotline: +358 412 345 023</span>
+              </ListGroupItem>
+
+              <ListGroupItem className='delivery__time-item border-0 ps-0'>
+                <span>Email: contact@foodie.com</span>
+              </ListGroupItem>
+            </ListGroup>
+          </Col>
+          <Col lg='3' md='4' sm='6'>
+            <h5 className='footer__title'>Newsletter</h5>
+            <p>Subcribe our newsletter</p>
+            <div className='newsletter'>
+              <input type='email' placeholder='Enter your email'></input>
+              <span>
+                <i class='ri-send-plane-line'></i>
+              </span>
+            </div>
+          </Col>
+        </Row>
+        <Row className='mt-5'>
+          <Col lg='6' md='6'>
+            <p className='copyright__text'>
+              Copyright - 2022, made by Tram Nguyen, under guidance from Muhibur
+              Rahman
+            </p>
+          </Col>
+          <Col lg='6' md='6'>
+            <div className='social__links d-flex align-items-center gap-4 justify-content-end'>
+              <p className='m-0'>Follow us: </p>
+              <span>
+                <Link to='https://www.facebook.com/tramngoc1999'>
+                  <i className='ri-facebook-circle-fill'></i>
+                </Link>
+              </span>
+              <span>
+                <Link to='https://github.com/tramnguyenhere'>
+                  <i className='ri-github-fill'></i>
+                </Link>
+              </span>
+              <span>
+                <Link to='https://www.linkedin.com/in/tramnguyenhere/'>
+                  <i className='ri-linkedin-box-fill'></i>
+                </Link>
+              </span>
+            </div>
+          </Col>
         </Row>
       </Container>
     </footer>
