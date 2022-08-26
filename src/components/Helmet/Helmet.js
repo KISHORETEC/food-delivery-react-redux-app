dececee08c478c8189204323c6947498f48e4376
Tram Nguyen <tramngoc1402@gmail.com>
2022-08-26 13:21:21 +0300

    Finished Homepage

--- src/components/Helmet/Helmet.js
+++ src/components/Helmet/Helmet.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Helmet = (props) => {
-  document.title = 'Food ordering app - ' + props.title;
+  document.title = props.title + ' - Foodie';
   return <div className='w-100'>{props.children}</div>;
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,67 @@
 import React from 'react';
+import Helmet from '../components/Helmet/Helmet.js';
+import { Container, Row, Col } from 'reactstrap';
+import heroImg from '../assets/images/hero.png';
+import '../styles/hero-section.css';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Helmet title='Home'>
+      <section>
+        <Container>
+          <Row>
+            <Col lg='6' md='6'>
+              <div className='hero__content'>
+                <h5 className='mb-3'>
+                  Fast and easy food delivery service to spoil the foodie within
+                  you
+                </h5>
+                <h1 className='mb-4 hero__title'>
+                  <span>HUNGRY?</span> just wait <br /> food at
+                  <span> your door!</span>
+                </h1>
+
+                <p>
+                  Check it out! Enjoy authentic Vietnamese local food with 60%
+                  discount and more.
+                </p>
+
+                <div className='hero__btns d-flex align-items-center gap-5 mt-4'>
+                  <button className='order__btn d-flex align-items-center justify-content-between'>
+                    Order now <i className='ri-arrow-right-s-line'></i>
+                  </button>
+
+                  <button className='all__foods-btn'>
+                    <Link to='/foods'>See all foods</Link>
+                  </button>
+                </div>
+                <div className='hero__service d-flex align-items-center gap-5 mt-5'>
+                  <p className='d-flex align-items-center gap-2'>
+                    <span className='shipping__icon'>
+                      <i className='ri-car-line'></i>
+                    </span>
+                    Free Delivery
+                  </p>
+                  <p className='d-flex align-items-center gap-2'>
+                    <span className='shipping__icon'>
+                      <i className='ri-shield-check-line'></i>
+                    </span>
+                    100% secure checkout
+                  </p>
+                </div>
+              </div>
+            </Col>
+            <Col lg='6' md='6'>
+              <div className='hero__img'>
+                <img src={heroImg} alt='hero-img' className='w-100' />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
 };
 
 export default Home;
